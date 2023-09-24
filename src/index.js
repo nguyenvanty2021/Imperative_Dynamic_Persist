ff683186800be64ad08376117577b803c2f94d1c
Nguyen Van Ty <anhhung_so11@yahoo.com>
2023-09-24 14:11:35 +0700

    clear warning

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
