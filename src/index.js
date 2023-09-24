455b605aef5db4c9774806d47e7ebc67946e1cbf
Nguyen Van Ty <anhhung_so11@yahoo.com>
2023-09-24 14:09:52 +0700

    revert dynamic

--- src/index.js
+++ src/index.js
@@ -6,26 +6,28 @@ import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-const App = lazy(() => import("./App"));
-const App1 = lazy(() => import("./App1"));
+import App from "./App";
+import App1 from "./App1";
+// const App = lazy(() => import("./App"));
+// const App1 = lazy(() => import("./App1"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <Suspense fallback={<div>Page is Loading...</div>}>
-        <Provider store={store}>
-          <PersistGate persistor={persistor}>
-            <Switch>
-              <Route path="/app">
-                <App1 />
-              </Route>
-              <Route path="/">
-                <App />
-              </Route>
-            </Switch>
-          </PersistGate>
-        </Provider>
-      </Suspense>
+      {/* <Suspense fallback={<div>Page is Loading...</div>}> */}
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Switch>
+            <Route path="/app">
+              <App1 />
+            </Route>
+            <Route path="/">
+              <App />
+            </Route>
+          </Switch>
+        </PersistGate>
+      </Provider>
+      {/* </Suspense> */}
     </Router>
   </React.StrictMode>
 );
