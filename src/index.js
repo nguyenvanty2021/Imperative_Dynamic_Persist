eaeb2cc4524bc64241c14eaadb3c39b660e68c8a
Nguyen Van Ty <anhhung_so11@yahoo.com>
2023-09-24 23:08:07 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -6,19 +6,19 @@ import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-import App from "./App";
-import App1 from "./App1";
+// import App from "./App";
+// import App1 from "./App1";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 // const App3 = lazy(() => import("./App3"));
 // const App4 = lazy(() => import("./App4"));
-// const App5 = lazy(() => import("./App5"));
-// const App6 = lazy(() => import("./App6"));
+const App5 = lazy(() => import("./App5"));
+const App6 = lazy(() => import("./App6"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
       <ul>
-        <li>
+        {/* <li>
           <Link to="/">Page A</Link>
         </li>
         <li>
@@ -29,7 +29,7 @@ root.render(
         </li>
         <li>
           <Link to="/d">Page D</Link>
-        </li>
+        </li> */}
         <li>
           <Link to="/e">Page E</Link>
         </li>
@@ -41,26 +41,26 @@ root.render(
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <Switch>
-            {/* <Suspense fallback={<div>Page is Loading...</div>}> */}
-            {/* <Route path="/d">
+            <Suspense fallback={<div>Page is Loading...</div>}>
+              {/* <Route path="/d">
                 <App4 />
               </Route>
               <Route path="/c">
                 <App3 />
-              </Route>
+              </Route> */}
               <Route path="/e">
                 <App5 />
               </Route>
               <Route path="/f">
                 <App6 />
               </Route>
-            </Suspense> */}
-            <Route path="/b">
+              {/* <Route path="/b">
               <App1 />
             </Route>
             <Route path="/">
               <App />
-            </Route>
+            </Route> */}
+            </Suspense>
           </Switch>
         </PersistGate>
       </Provider>
